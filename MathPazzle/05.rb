48c80c3ed5a8a6da41f9af53dc3c77a9ca839c95
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2018-07-01 02:17:04 +0900

    05の別解を追加

--- MathPazzle/05.rb
+++ MathPazzle/05.rb
@@ -1,16 +1,28 @@
 Price = 1000
 CoinNum = 15
 
-pattern = 0
+# pattern = 0
 
-for i10 in 0..15 do
-  for i50 in 0..(15 - i10) do
-    for i100 in 0..(15 - i10 - i50) do
-      for i500 in 0..(15 - i10 - i50 - i100) do
-        pattern += 1 if 10 * i10 + 50 * i50 + 100 * i100 + 500 * i500 == 1000
-      end
-    end
-  end
+# for i10 in 0..15 do
+#   for i50 in 0..(15 - i10) do
+#     for i100 in 0..(15 - i10 - i50) do
+#       for i500 in 0..(15 - i10 - i50 - i100) do
+#         pattern += 1 if 10 * i10 + 50 * i50 + 100 * i100 + 500 * i500 == 1000
+#       end
+#     end
+#   end
+# end
+
+# p pattern
+
+# 別解
+
+coins = [10, 50, 100, 500]
+pattern = 0
+(2..CoinNum).each do |i|
+  coins.repeated_combination(i).each{|coin_set|
+    pattern += 1 if coin_set.inject(:+) == 1000
+  }
 end
 
 p pattern
